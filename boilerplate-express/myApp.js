29ccacba0fdc09dfe223ce720c8284c099f49868
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-09-18 22:09:20 +0530

    Basic Node and Express - transfering from replit

---
+++ boilerplate-express/myApp.js
@@ -0,0 +1,49 @@
+let express = require('express');
+let app = express();
+
+app.get(
+  "/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({message: "HELLO JSON"});
+    } else {
+      res.json({message: "Hello json"});
+    }
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
